13dbcbc37ee5e15aa33bcca3dca479f61954581c
Christian von Stebut <christian@von.stebut.org>
2020-03-21 13:22:42 +0100

    added tests

--- hellogo.go
+++ hellogo.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/cvstebut/hellogo/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("Hello, world. Two. Added to repo."))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
